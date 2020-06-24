5facf87228c24d2580c0c60f77a8e3f5c1f3e6c5
elnur norzalov <norzalov.elnur@gmail.com>
2020-06-24 22:45:30 +0400

    structure change

---
+++ client/build/precache-manifest.728d452ecbf84ce8e8febe41b26f20f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66447f0a936ba1482609d900a3a9681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b7f91ebb0ed41a03a5f",
+    "url": "/static/css/2.6a1616ae.chunk.css"
+  },
+  {
+    "revision": "3b7f91ebb0ed41a03a5f",
+    "url": "/static/js/2.0900de94.chunk.js"
+  },
+  {
+    "revision": "1bc80f6a1e8aacda9b18c4bc7a08babc",
+    "url": "/static/js/2.0900de94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7a1c0b0b1f6ba95681",
+    "url": "/static/js/main.0d0e847d.chunk.js"
+  },
+  {
+    "revision": "132e6df77342523be385",
+    "url": "/static/js/runtime-main.d1cc60de.js"
+  },
+  {
+    "revision": "52bfab3c9e6fbbec1f563bc6f1aa6dfa",
+    "url": "/static/media/C++.52bfab3c.svg"
+  },
+  {
+    "revision": "5a3866b5f5d731afa0318796b5c87dd9",
+    "url": "/static/media/C.5a3866b5.svg"
+  },
+  {
+    "revision": "e6d87e2dca08c953fe3acd1275ed4f4e",
+    "url": "/static/media/Java.e6d87e2d.svg"
+  },
+  {
+    "revision": "ba0e8b29f29540094418f8230ab0887e",
+    "url": "/static/media/Nodejs.ba0e8b29.svg"
+  },
+  {
+    "revision": "3e7935c31a43675678bc82bfc961a252",
+    "url": "/static/media/Python.3e7935c3.svg"
+  },
+  {
+    "revision": "be50714d848d9be8acf79504f03ed939",
+    "url": "/static/media/coding.be50714d.jpg"
+  }
+]);
