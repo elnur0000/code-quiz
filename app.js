dd2600722ff15386347acd3c358ed63e9e16a3c0
elnur norzalov <norzalov.elnur@gmail.com>
2020-06-24 22:50:45 +0400

    structural changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Middleware = require('./middleware/middleware')
 const ErrorHandlingMiddleware = require('./middleware/error-handling')
 const connectDB = require('./config/db')
+const path = require('path')
 
 // Connect to database if it's not a test environment
 if (process.env.NODE_ENV !== 'test') connectDB()
@@ -20,6 +21,12 @@ app.use('/api/v1/auth', auth)
 app.use('/api/v1/problems', problem)
 app.use('/api/v1/groups', group)
 app.use('/api/v1/tests', test)
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 ErrorHandlingMiddleware(app)
 
